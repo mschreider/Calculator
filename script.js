da21bbe792aa697a72b75ca847ba0f09a604919c
Matt Schreider <matt.schreider@gmail.com>
2020-06-04 00:31:49 -0400

    added html content

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var start_value = 0;
 function add(numbers) {
     // let sum = 0;
     // for(let x = 0; x<numbers.length; x++){
@@ -5,31 +6,55 @@ function add(numbers) {
     // }
     // console.log(sum);
     // return sum;
-    let sum = numbers.reduce((total, num) => total + num);
+    let sum = numbers.reduce(((total, num) => total + num), start_value);
     console.log(sum);
+    start_value = sum;
     return sum; 
 }
 
 function subtract(numbers) {
-    let difference = numbers.reduce((total, num) => total - num);
+    let difference = numbers.reduce(((total, num) => total - num), start_value);
     console.log(difference);
+    start_value = difference;
     return difference;
 }
 
 function multiply(numbers) {
-    let product = numbers.reduce((total, num) => total * num);
+    start_value = 1;
+    let product = numbers.reduce(((total, num) => total * num), start_value);
     console.log(product);
+    start_value = product;
     return product;
 }
 
 function divide(numbers) {
-    let quotient = numbers.reduce((total, num) => total / num);
+    start_value = 1;
+    let quotient = numbers.reduce(((total, num) => total / num), start_value);
     console.log(quotient);
+    start_value = quotient;
     return quotient;
 }
 
-numbers = [8,4,2];
-add(numbers);
-subtract(numbers);
-multiply(numbers);
-divide(numbers);
+// Create a new function operate that takes an operator 
+// and 2 numbers and then calls one of the above functions on the numbers.
+function operate(operator, numbers){
+    switch (operator) {
+        case '*':
+            multiply(numbers);
+            break;
+    
+        case '/':
+            divide(numbers);
+            break;
+        
+        case '+':
+            add(numbers);
+            break;
+        
+        case '-':
+            subtract(numbers);
+            break;
+    }
+}
+
+operate('/', [1,2,3,4,5]);
